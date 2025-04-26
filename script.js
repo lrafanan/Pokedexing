ff72d18201bfc74c0b69e65be7ec63ec0c7485e4
Lyanne Marie Rafanan <lrafanan1@csub.edu>
2025-04-26 15:57:53 -0700

    added display.html to display desired pokemon, changed button to redirect to new page, added logic for the display.html to work

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+fetchData(); // Lyanne - used to show the pokemon on display.html
+
+// Lyanne's function - Redirects to display.html with the pokemonName inputed by the user through the URL
+async function openDisplay() {
+    const text = document.getElementById("pokemonName").value;
+    const encodedText = encodeURIComponent(text);
+    
+    window.open(`display.html?data=${encodedText}`, "_blank");
+}
+
 async function fetchData() // Mauro did this whole function
 {
+    // Lyanne - Added these lines to get the Name through the URL 
+    const params = new URLSearchParams(window.location.search);
+    const pokemonName = params.get("data"); 
+
     try
     {
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        //const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if(!response.ok)
         {
+            document.getElementById("pokemonName").textContent = pokemonName + " is not real."; // Lyanne - Added some Error Handling
             throw new Error("Could not fetch resource");
+
         }
 
         const data = await response.json();
+        
         const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemonSprite");
-
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
+        // Lyanne - Displays name of Pokemon (will display more elements later from the API)
+        const nameElement = document.getElementById("pokemonName");
+        nameElement.textContent = data.forms[0].name;
+
     }
     catch(error)
     {
